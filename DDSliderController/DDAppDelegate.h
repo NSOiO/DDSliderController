5c34b5df84bb411f81e9fed7f656edc51dfcac62
handaer <daition340@163.com>
2014-08-13 17:02:18 +0800

    first commit

---
+++ DDSliderController/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DDSliderController
+//
+//  Created by handaer on 14-8-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
