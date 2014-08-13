e5f5134ec71973cd17bbec4061ca1870dc185eb6
handaer <daition340@163.com>
2014-08-13 17:21:15 +0800

    first commit

---
+++ DDSliderControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DDSliderControllerDemo
+//
+//  Created by handaer on 14-8-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
